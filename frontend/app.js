403028e54310df80bd4f8cd9a5f250daf8d2c5ca
rahulyadav108 <ry1975908@gmail.com>
2025-04-28 11:50:14 +0530

    Delete frontend/app.js

--- frontend/app.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('ctaButton').addEventListener('click', function() {
-    alert("Redirecting to sign-up page...");
-    // Here, you can add a redirect to a sign-up page or another action
-});
