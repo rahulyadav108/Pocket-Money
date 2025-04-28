fdfea22dc42642008f4d9f022f0ca5637e08edab
rahulyadav108 <ry1975908@gmail.com>
2025-04-28 11:44:17 +0530

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,4 @@
+document.getElementById('ctaButton').addEventListener('click', function() {
+    alert("Redirecting to sign-up page...");
+    // Here, you can add a redirect to a sign-up page or another action
+});
